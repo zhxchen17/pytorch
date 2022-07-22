67b42a9bbf4891f06f982eb6dc71bf66c8e2627b
John Clow <jclow@fb.com>
2022-07-22 15:47:36 -0700

    [TorchTidy] Adding support for storing scalar values in profiling
    ghstack-source-id: 3a84c19b2942f92934d620d7cc109a21116cf8a6
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/81843

--- torch/csrc/autograd/init.cpp
+++ torch/csrc/autograd/init.cpp
@@ -284,8 +284,17 @@ PyObject* THPAutograd_initExtension(PyObject* _unused, PyObject* unused) {
             "allow_tf32_cublas",
             &ExtraFields<EventType::TorchOp>::allow_tf32_cublas_);
     py::class_<Inputs>(m, "_Inputs")
-        .def_readonly("tensor_metadata", &Inputs::tensor_metadata_)
-        .def_readonly("dtypes", &Inputs::dtypes_);
+        .def_readonly("dtypes", &Inputs::dtypes_)
+        .def_property_readonly(
+            "ivalues",
+            [](const Inputs& inputs) {
+              py::list list;
+              for (auto& v : inputs.ivalues_) {
+                list.append(torch::jit::toPyObject(v));
+              }
+              return list;
+            })
+        .def_readonly("tensor_metadata", &Inputs::tensor_metadata_);
 
     py::class_<TensorMetadata>(m, "_TensorMetadata")
         .def_property_readonly(
