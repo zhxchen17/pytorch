5f7c9962af3d3c0c00f41cab535e3a06f1e0bedd
Fabio Rocha <frocha@quansight.com>
2022-07-25 11:57:07 +0000

    [primTorch] Added unbind OpInfo and ref
    ghstack-source-id: 1322ba3b2805424842186cf8993b694b673d3028
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/81776

--- torch/_refs/__init__.py
+++ torch/_refs/__init__.py
@@ -221,6 +221,7 @@ __all__ = [
     "view",
     "vsplit",
     "vstack",
+    "unbind",
     #
     # Tensor Creation
     #
@@ -2673,6 +2674,19 @@ def vstack(tensors: TensorSequenceType) -> TensorLikeType:
     check(len(tensors) > 0, lambda: "vstack expects a non-empty TensorList")
     aligned_tensors = atleast_2d(*tensors)
     return cat(aligned_tensors, 0)
+
+
+@register_decomposition(torch.ops.aten.unbind)
+def unbind(t: TensorLikeType, dim: int = 0) -> TensorSequenceType:
+    dim = utils.canonicalize_dim(t.ndim, dim)
+    check(
+        len(t.shape) > 0,
+        lambda: "dimension specified as 0 but tensor has no dimensions",
+        IndexError,
+    )
+    return tuple(
+        torch.squeeze(s, dim) for s in torch.tensor_split(t, t.shape[dim], dim)
+    )
 
 
 # Note: although squeeze is documented as having the out= kwarg it doesn't
